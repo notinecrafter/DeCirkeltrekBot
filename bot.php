14c16aa008b6ab0f3693ea7defe7f55d2801dbf2
Maartenwut <maartenn2001@gmail.com>
2016-07-31 12:11:17 +0200

    Update bot.php

--- bot.php
+++ bot.php
@@ -261,7 +261,7 @@ else if (strlen(strstr($text,"/doei"))>0 || strlen(strstr($text,"/dag"))>0) {
 }
 
 //ik_ihe
-else if (strlen(strstr($text,"/doei"))>0 || strlen(strstr($text,"/ik_ihe"))>0) {
+else if (strlen(strstr($text,"/ik_ihe"))>0) {
 	$telegram->sendMessage(array('chat_id' => $chat_id, 'text' => output_rss_feed('https://www.reddit.com/r/ik_ihe/new/.rss')));
 }
 
