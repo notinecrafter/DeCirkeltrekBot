33b465e29af281ad90f69f8eff35c0ba34fd1536
Maartenwut <maartenn2001@gmail.com>
2016-07-31 19:12:16 +0200

    Update bot.php

--- bot.php
+++ bot.php
@@ -262,6 +262,16 @@ else if (strlen(strstr($text,"/angelsaksisch"))>0) {
 	$telegram->sendPhoto(array('chat_id' => $chat_id, 'photo' => new CURLFile("./assets/angelsaksisch.jpg")));
 }
 
+//hoekig
+else if (strlen(strstr($text,"/hoekig"))>0) {
+	$telegram->sendPhoto(array('chat_id' => $chat_id, 'photo' => new CURLFile("./assets/hoekig.jpg")));
+}
+
+//goedverhaal
+else if (strlen(strstr($text,"/goedverhaal"))>0) {
+	$telegram->sendPhoto(array('chat_id' => $chat_id, 'photo' => new CURLFile("./assets/goedverhaal.jpg")));
+}
+
 //levededevs
 else if (strlen(strstr($text,"/levededevs"))>0) {
     $telegram->sendMessage(array('chat_id' => $chat_id, 'text' => "Ik ben gemaakt door @Maartenwut met overgeporte code van de oude @FlippyBot gemaakt door @Flippylosaurus. \xF0\x9F\x98\x84 Ik sta op github. https://github.com/Maartenwut/LeveDeKoningBot-Telegram"));
