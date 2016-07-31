d0ff8247c79c5855915674e9d3bee4a186676ac5
Maartenwut <maartenn2001@gmail.com>
2016-07-31 19:19:22 +0200

    Update bot.php

--- bot.php
+++ bot.php
@@ -275,7 +275,7 @@ else if (strlen(strstr($text,"/goedverhaal"))>0) {
 //zucht
 else if (strlen(strstr($text,"/zucht"))>0) {
 	$telegram->sendSticker(array('chat_id' => $chat_id, 'sticker' => 'BQADBAADEwADbkngC6ANL5qYiJcwAg' ));
-}BQADBAADAQEAAtlNiQcKdjaIYPZ0qAI
+}
 
 //kansloos
 else if (strlen(strstr($text,"/kansloos"))>0) {
