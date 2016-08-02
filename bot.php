d0fae934edba8cda5f6e931773096ae23f154e67
Maarten Dekkers <maartenn2001@gmail.com>
2016-08-02 12:14:25 +0200

    Update bot.php

--- bot.php
+++ bot.php
@@ -400,10 +400,12 @@ else if (strlen(strstr($text,"/levededevs"))>0) {
 }
 
 //commands
-else if ($telegram->messageFromGroup() == true) {
-    $telegram->sendMessage(array('chat_id' => $chat_id, 'text' => "Doe dat maar niet in een groep.", 'reply_to_message_id' => $telegram->MessageID()));
-} else if ($telegram->messageFromGroup() == false) {
-    $telegram->sendMessage(array('chat_id' => $chat_id, 'text' => file_get_contents('./ignore/commands.txt')));
+else if (strlen(strstr($text,"/commands"))>0){
+	if ($telegram->messageFromGroup() == true) {
+	    $telegram->sendMessage(array('chat_id' => $chat_id, 'text' => "Doe dat maar niet in een groep.", 'reply_to_message_id' => $telegram->MessageID()));
+	} else if ($telegram->messageFromGroup() == false) {
+	    $telegram->sendMessage(array('chat_id' => $chat_id, 'text' => file_get_contents('./ignore/commands.txt')));
+	}
 }
 else if ($telegram->person() != false){
 	if ($telegram->person() == 'new') {
