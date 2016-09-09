f9e6aa1f3196dc7e219f7f8f21540b90fea45dcc
Maarten Dekkers <maartenn2001@gmail.com>
2016-09-09 23:22:05 +0200

    Update bot.php

--- bot.php
+++ bot.php
@@ -435,6 +435,11 @@ else if (strlen(strstr($text,"/excuses"))>0) {
 	$telegram->sendPhoto(array('chat_id' => $chat_id, 'photo' => new CURLFile("./assets/excuses.jpg")));
 }
 
+//siebe
+else if (strlen(strstr($text,"/siebe"))>0) {
+	$telegram->sendPhoto(array('chat_id' => $chat_id, 'photo' => new CURLFile("./assets/siebe.jpg")));
+}
+
 //levededevs
 else if (strlen(strstr($text,"/levededevs"))>0) {
     $telegram->sendMessage(array('chat_id' => $chat_id, 'text' => "Ik ben gemaakt door @Maartenwut met overgeporte code van de oude @FlippyBot gemaakt door @Flippylosaurus. \xF0\x9F\x98\x84" . PHP_EOL . "Ik sta op github. https://github.com/Maartenwut/DeCirkeltrekBot"));
